d52d67687bf8d628ecfc0e01ebb077c4ad23a6fb
Li Ouyang <li.ouyang@gmail.com>
2012-09-20 02:18:08 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sixtimesbook::Application.config.secret_token = '9535d72274b2b8da788f2f52081fb4fd43f308392ab2e3a1901415183b7d7bcc4152b9ea3a5705d72b422a94d25421098a35643aeadb108b94162591404e6005'
