8f1e0e7be6a0388dde2a8c99d97d80ba329990f9
Li Ouyang <li.ouyang@gmail.com>
2012-09-20 13:53:08 -0400

    set up most of authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Sixtimesbook::Application.routes.draw do
   get "sessions/new"
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  root :to => "users#new"
 
   resources :users
 
@@ -56,7 +60,7 @@ Sixtimesbook::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'entries#new'
+
 
   # See how all your routes lay out with "rake routes"
 
